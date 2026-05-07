3bd78142f4962839835b8ef0bf150204883470c9
osdopllamadas <osdopllamadas@users.noreply.github.com>
2026-05-07 00:11:00 -0400

    chore: ignore local data and scratch files

--- scratch/remove_div.ps1
+++
@@ -1,8 +0,0 @@
-$c = Get-Content 'd:\esden-dashboard\src\app\dashboard\agents\page.tsx'
-$newC = @()
-for ($i=0; $i -lt $c.Length; $i++) {
-    if ($i -ne 756) { # Line 757 is index 756
-        $newC += $c[$i]
-    }
-}
-$newC | Set-Content 'd:\esden-dashboard\src\app\dashboard\agents\page.tsx'
